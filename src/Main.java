eacea54ad71c62c2995e16092e07e7b4b214251c
Christina Kungurtseva <kungurts@gmail.com>
2022-11-21 19:37:39 +0300

    option delete < 18 added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,26 +11,15 @@ public class Main {
         people.add(new Person("Alex", "Picasso", 35));
         people.add(new Person("Kim", "Al Yo Pachino", 14));
         people.add(new Person("Jack", "Do Shi Rak", 18));
+        people.add(new Person("Li", "Kooper", 16));
 
         System.out.println(people);
 
-        Comparator<Person> comparator = (p1, p2)  -> {
-            int wordsLimitInSurname = 2;
-            if ((p1.surnameSplit() <= wordsLimitInSurname) && (p2.surnameSplit() <= wordsLimitInSurname)) {
-                if (p1.surnameSplit() > p2.surnameSplit()) {
-                    return 1;
-                } else if (p1.surnameSplit() < p2.surnameSplit()) {
-                    return -1;
-                } else {
-                    return Integer.compare(p1.getAge(), p2.getAge());
-                }
-            }
-            else {
-                return Integer.compare(p1.getAge(), p2.getAge());
-            }
-        };
+//        Predicate<Person> personRemove = person -> person.getAge() < 18;
+//        people.removeIf(personRemove);
 
-        Collections.sort(people, comparator);
+        people.removeIf(person -> person.getAge() < 18);
+
         System.out.println(people);
     }
 }
